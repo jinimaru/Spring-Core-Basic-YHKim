15cc0b62017a729024361a80659b66d74c197142
jinimaru <dev.sangjin.lee@gmail.com>
2022-07-12 11:13:19 -0300

    refactor: Edit AppConfig

--- src/main/java/com/study/yhkim/springcore/AppConfig.java
+++ src/main/java/com/study/yhkim/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package com.study.yhkim.springcore;
 
+import com.study.yhkim.springcore.discount.DiscountPolicy;
 import com.study.yhkim.springcore.discount.FixDiscountPolicy;
+import com.study.yhkim.springcore.member.MemberRepository;
 import com.study.yhkim.springcore.member.MemberService;
 import com.study.yhkim.springcore.member.MemberServiceImpl;
 import com.study.yhkim.springcore.member.MemoryMemberRepository;
@@ -9,6 +11,10 @@ import com.study.yhkim.springcore.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    /*
+    (First Code)
+    역할과 구현 클래스가 잘 보이지 않음
+    코드 중복: new MemoryMemberRepository()
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
@@ -16,4 +22,27 @@ public class AppConfig {
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
+    */
+
+    /*
+    (Refactoring)
+    코드 중복을 제거하고 역할과 구현이 보이도록 리펙터링
+     - 메서드 이름을 보면 역할(Implements)이 드러나고, 구현(Class)이 보인다.
+    */
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
 }
