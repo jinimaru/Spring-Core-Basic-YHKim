44c684d3cabf3e68ceacd5779569a866a394a83b
jinimaru <dev.sangjin.lee@gmail.com>
2022-07-12 22:55:35 -0300

    test: Spring bean container reg. through XML file
    appConfig.xml

---
+++ src/test/java/com/study/yhkim/springcore/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package com.study.yhkim.springcore.xml;
+
+import com.study.yhkim.springcore.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    @DisplayName("XML 빈 설정 정보를 통한 스프링 빈 컨테이너 등록")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
